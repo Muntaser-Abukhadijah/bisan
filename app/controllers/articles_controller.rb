3d8cdcfff5db72e37a4252f3afae5fe9e23968a1
Muntaser Abukhadijah <abukhadijah.muntaser@gmail.com>
2025-08-27 14:47:54 +0300

    Added meilisearch and search bar to articles page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,17 @@
 class ArticlesController < ApplicationController
+  include Pagy::Backend
+
   def index
-    @pagy, @articles = pagy(Article.all)
+    @q = params[:q].to_s.strip
+
+    hits = if @q.present?
+      Article.pagy_search(@q, hits_per_page: 12, sort: [ "created_at:desc" ])
+    else
+      # show everything when no query; "*" means match all
+      Article.pagy_search("*", hits_per_page: 12, sort: [ "created_at:desc" ])
+    end
+
+    @pagy, @articles = pagy_meilisearch(hits)
   end
 
   def show
