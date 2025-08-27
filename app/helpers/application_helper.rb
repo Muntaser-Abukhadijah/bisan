31255172b79e2eb2598dd549d8c10bb9b3624182
Muntaser Abukhadijah <abukhadijah.muntaser@gmail.com>
2025-08-27 16:02:50 +0300

    Added RTL support

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  RTL_LOCALES = %i[ar].freeze
+  def page_dir(locale = I18n.locale)
+    RTL_LOCALES.include?(locale.to_sym) ? "rtl" : "ltr"
+  end
 end
